cbda5aba8265d648e2703ee46e615e56d45e7d9a
Dhar <dhar@fyles-MacBook-Air.local>
2021-04-27 17:19:33 +0530

    Updated PR

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ window.onload = function () {
 
     var quickFyle1 = Quickfyle(element1, { backgroundColor: 'red' });
     var quickFyle2 = Quickfyle(element2, {
-        backgroundColor: 'green', onPrimaryClickedHook: function (args) {
+        backgroundColor: 'green',
+        onPrimaryClickedHook: function (args) {
             console.log(args);
         }
     });
-    var quickFyle3 = Quickfyle(element3, { backgroundColor: 'blue', onSecondaryClickedHook: function (args) {
-        console.log(args);
-    } });
-
+    var quickFyle3 = Quickfyle(element3, {
+        backgroundColor: 'blue', onSecondaryClickedHook: function (args) {
+            console.log(args);
+        }
+    });
 
     quickFyle1.getData(123).then(console.log);
-
-    //    quickFyle1.openModal();
-    //    setTimeout(()=> {
-    //        quickFyle1.closeModal();
-    //    }, 5000);
 }
